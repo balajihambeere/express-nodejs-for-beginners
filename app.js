f7920252fd47cb8d8166a8a808d38a9e7cb2576f
balaji hambeere <balaji.hambeere@hotmail.com>
2018-09-15 10:09:34 +0530

    Added Project to repository

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var routes = require('./src/routes/appRoutes');
+
+var app = new express();
+const PORT = 3000;
+
+mongoose.Promise = global.Promise;
+const options = { useNewUrlParser: true };
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds157762.mlab.com:57762/<databasename>', options,
+    function (error) {
+        if (error) { console.log(error); }
+    });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+routes(app);
+app.get('/', (req, res) =>
+    res.send(`Server is running on port ${PORT}`)
+);
+app.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`)
+);
---
+++ src/controllers/customerController.js
@@ -0,0 +1,59 @@
+var mongoose = require('mongoose');
+var CustomerSchema = require('../models/customerModel');
+
+const Customer = mongoose.model('Customer', CustomerSchema);
+
+const createCustomer = (request, response) => {
+    let customer = new Customer(request.body);
+    customer.save((error, customer) => {
+        if (error) {
+            response.send(err);
+        }    
+        response.json(customer);
+    })
+}
+
+const getCustomers = (request, response) => {
+    Customer.find({}, (error, customer) => {
+        if (error) {
+            response.send(err);
+        }
+        response.json(customer);
+    });
+}
+
+const getCustomerById = (request, response) => {
+    Customer.findById({ _id: request.params.customerId }, (error, customer) => {
+        if (error) {
+            response.send(err);
+        }
+        response.json(customer)
+    });
+}
+
+const updateCustomer = (request, response) => {
+    Customer.findOneAndUpdate({ _id: request.params.customerId }, request.body, { new: true },
+        (error, customer) => {
+            if (error) {
+                response.send(err);
+            }
+            response.json(customer);
+        });
+}
+
+const deleteCustomer = (request, response) => {
+    Customer.remove({ _id: request.params.customerId }, (error, customer) => {
+        if (error) {
+            response.send(err);
+        }
+        response.json(customer);
+    });
+}
+
+module.exports = {
+    getCustomers,
+    createCustomer,
+    getCustomerById,
+    updateCustomer,
+    deleteCustomer
+}
