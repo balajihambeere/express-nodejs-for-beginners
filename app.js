c225e8dc55b8789da3c88537341c93662e775753
balaji hambeere <balaji.hambeere@hotmail.com>
2018-09-15 10:25:57 +0530

    Added npm packages

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ var bodyParser = require('body-parser');
 var routes = require('./src/routes/appRoutes');
 
 var app = new express();
+
 const PORT = 3000;
 
 mongoose.Promise = global.Promise;
+
 const options = { useNewUrlParser: true };
+
 mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds157762.mlab.com:57762/<databasename>', options,
     function (error) {
         if (error) { console.log(error); }
     });
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
+
 routes(app);
+
 app.get('/', (req, res) =>
     res.send(`Server is running on port ${PORT}`)
 );
+
 app.listen(PORT, () =>
     console.log(`Server is running on port ${PORT}`)
 );
--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,10 +1,15 @@
 var customer = require('../controllers/customerController');
 
 const routes = (app) => {
+    
     app.route('/customers').get(customer.getCustomers);
+
     app.route('/customer').post(customer.createCustomer);
+    
     app.route('/customer/:customerId').get(customer.getCustomerById);
+    
     app.route('/customer/:customerId').put(customer.updateCustomer);
+    
     app.route('/customer/:customerId').delete(customer.deleteCustomer);
 }
 module.exports = routes;
